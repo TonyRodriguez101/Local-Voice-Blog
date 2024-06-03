2b5c7dd5a596482e1dece82e2824aed2fbdc56f6
TonyRodriguez101 <antonioecr95@gmail.com>
2024-06-03 05:32:41 +0100

    v2

--- blog/Local_Voice/models.py
+++ blog/Local_Voice/models.py
@@ -4,9 +4,17 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Post(models.Model):
-    title=models.CharField(max_length=255)
-    author=models.ForeignKey(User, on_delete=models.CASCADE) #esto sirve para si eliminamos un usuario borrara todos sus post.
-    body=models.TextField()
+    title = models.CharField(max_length=255)
+    #header_image = models.ImageField(null=True, blank=True, upload_to="images/")
+    title_tag = models.CharField(max_length=255, default="Local_Voice")
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    #body = RichTextField(blank=True, null=True)
+    body = models.TextField()
+    #post_date = models.DateField(auto_now_add=True)
+    #category = models.CharField(max_length=255, default='coding')
+    #snippet = models.CharField(max_length=255)
+    #likes = models.ManyToManyField(User, related_name='blog_posts')
+
 
 
     def __str__(self):
